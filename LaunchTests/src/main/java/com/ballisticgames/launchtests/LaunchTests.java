aaeff056fe2520936609a1823e25f5ce8ac8772b
Owen Rossi-Keen <owenrossikeen@gmail.com>
2022-04-26 16:04:01 -0400

    Minor changes and file-paths configured

---
+++ LaunchTests/src/main/java/com/ballisticgames/launchtests/LaunchTests.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.ballisticgames.launchtests;
+
+/**
+ *
+ * @author owenr
+ */
+public class LaunchTests {
+
+    public static void main(String[] args) {
+        System.out.println("Very epic!");
+    }
+}
